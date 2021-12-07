efadcee870225f8d565a92bb48c71ba696037889
chucklind <yitzchak.lindenbaum@mail.huji.ac.il>
2021-12-07 21:02:06 +0200

    Initial commit

---
+++ src/ascii_art/Driver.java
@@ -0,0 +1,15 @@
+package ascii_art;
+
+/**
+ * Managing function of ascii art enterprise.
+ */
+public class Driver {
+
+    /**
+     * Main function to run.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
