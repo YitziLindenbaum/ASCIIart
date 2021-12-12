1e5d008b67ecc5ecb9bea13d703fa96d21822992
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2021-12-12 20:12:43 +0200

    Added blank API

---
+++ src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
@@ -0,0 +1,18 @@
+package ascii_art.img_to_char;
+
+import image.Image;
+
+public class BrightnessImgCharMatcher {
+        
+        public BrightnessImgCharMatcher(Image img, String fontName){}
+        
+        public char[][] chooseChars(int numCharsInRow, Character[] charSet) {
+                char[][] ret = {{'0'}};
+                return ret;
+        }
+        
+        private char[][] convertImageToAscii() {
+                char[][] ret = {{'0'}};
+                return ret;
+        }
+}
